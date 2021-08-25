98f7e857ceba428248e619257230aec1cfc8de00
dewey <hammersdrichard@gmail.com>
2021-08-25 10:18:43 -0700

    commit tree 419b7f3391338e6c1b2486626ce65080fb0e8038

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,19 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => {
+        return (
+          <div key={Math.random()}>
+            <h2>{director.name}</h2>
+            <p>Movies:</p>
+            <ul>
+              {director.movies.map(moive => {
+                return <li key={Math.random()}>{moive}</li> 
+              })}
+            </ul>
+          </div>
+        )})}
     </div>
   );
 }
